c62896bb9b8e9ec494ae6195e05879ae3e157ca5
Daxon-ai <lestamediake@gmail.com>
2025-06-25 14:20:48 +0300

    Update uploadImage.js

--- AssetsClairity/library/uploadImage.js
+++ AssetsClairity/library/uploadImage.js
@@ -14,11 +14,11 @@ module.exports = async (buffer) => {
   const { ext } = await fromBuffer(buffer);
   let form = new FormData();
   form.append("file", buffer, "tmp." + ext);
-  let res = await fetch("https://telegra.ph/upload", {
+  let res = await fetch("https://files.catbox.moe/6afvpo.jpg", {
     method: "POST",
     body: form,
   });
   let img = await res.json();
   if (img.error) throw img.error;
-  return "https://telegra.ph" + img[0].src;
+  return "https://files.catbox.moe/6afvpo.jpg" + img[0].src;
 };
